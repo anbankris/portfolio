95246c13cd7102470081f19a33ef3458881b8ab6
Anbankris Prakasam <anban98@gmail.com>
2024-03-28 23:35:54 -0500

    feat: Simple Navbar & Name Animation and bio

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,6 @@
+"use client"
 import React from 'react'
-
+import { TypeAnimation } from 'react-type-animation'
 const HeroSection = () => {
   return (
     <section>
@@ -11,11 +12,13 @@ const HeroSection = () => {
           <br></br>
           <TypeAnimation
             sequence={[
-              "Judy",
+              "Anbankris Prakasam",
               1000,
               "Software Engineer",
               1000,
               "Full Stack Developer",
+              1000,
+              "DevOps Engineer",
               1000,
             ]}
             wrapper="span"
@@ -23,31 +26,9 @@ const HeroSection = () => {
             repeat={Infinity}
           />
         </h1>
-        <p className="text-[#ADB7BE] mb-6 textl-lg lg:text-xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-          voluptatum.
+        <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+          Currently pursuing my Masters in Management Information Systems at Texas A&M University. Almost 3 years of professional expericence in the Software Development Industry with primary focus on DevOps and Cloud Computing.
         </p>
-        <div>
-          <button className="bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white px-6 py-3 rounded-full mr-4">
-            Hire Me
-          </button>
-          <button className="m-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 px-1 py-1  text-white rounded-full">
-            <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-              Download CV
-            </span>
-          </button>
-        </div>
-      </div>
-      <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-        <div className="bg-[#181818] lg:w-[400px] lg:h-[400px] w-[250px] h-[250px] rounded-full relative">
-          <Image
-            // src="/images/hero_image.png"
-            alt="hero image"
-            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            width={300}
-            height={300}
-          />
-        </div>
       </div>
     </section>
   )
